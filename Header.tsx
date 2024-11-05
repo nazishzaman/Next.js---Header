68a2c38f8f25e5fe5ba5aa02ddc0637b8c512cba
nazishzaman <168236857+nazishzaman@users.noreply.github.com>
2024-11-05 07:41:51 -0800

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center bg-red-800 h-8 w-11'>
+        <h1>Father of the nation</h1>
+    </div>
+  )
+}
+
+export default Header;
